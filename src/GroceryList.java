1ad5a3dc48371c7d7405d9299c215e9b9ddcbbd6
Alexandra <fossum.alexandra@gmail.com>
2019-05-24 09:05:11 -0500

    added grocery list exercise

---
+++ src/GroceryList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GroceryList {
+    public static void main(String[] args) {
+        ArrayList<String> grocery = new ArrayList<String>();
+        Scanner sc = new Scanner (System.in);
+
+        String itemAdd = sc.next();
+        String userCont = sc.next();
+        int itemCount = sc.nextInt();
+
+        do {
+
+
+
+        } while (userCont != "yes");
+
+
+    }
+}
