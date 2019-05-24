25e47b15ba32732336e31e0a82f442a475ba51f5
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-05-24 09:56:18 -0500

    created baked goods and dairy class

---
+++ src/BakedGoods.java
@@ -0,0 +1,35 @@
+public class BakedGoods {
+    private String[] products;
+
+    public BakedGoods(String[] products) {
+        this.products = products;
+    }
+
+    public String[] getProducts() {
+        return products;
+    }
+
+    public void setProducts(String[] products) {
+        this.products = products;
+    }
+
+    public void callBakedList(){
+        String[] products={
+                "Biscuit ",
+                "Bread",
+                "Bagel ",
+                "Bun",
+                "Flatbread",
+                "Muffibn",
+                "Brownie",
+                "Cake"
+        };
+
+
+        for(String d: products){
+            System.out.println(d);
+        }
+        System.out.println("Please enter an item and an amount:");
+    }
+
+}
